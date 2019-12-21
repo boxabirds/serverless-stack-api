4b2af829cd429dac7fce2b56ea1897e9804bb7eb
Julian Harris <julian@disco-daddy.lan>
2019-12-21 23:42:02 +0000

    nodeId => noteId

--- update.js
+++ update.js
@@ -1,18 +1,20 @@
 import * as dynamoDbLib from './libs/dynamodb-lib'
 import { success, failure } from './libs/response-lib'
 
-export async function main (event, context, callback) {
+export async function main (event, context) {
   const data = JSON.parse(event.body)
+
+
   const params = {
     TableName: process.env.tableName,
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      nodeId: event.pathParameters.id
+      userId: {"S": event.requestContext.identity.cognitoIdentityId},
+      noteId: {"S": event.pathParameters.id}
     },
-    UpdateExpression: 'SET content = :content, attachment = :attachment',
+    UpdateExpression: "SET content = :content, attachment = :attachment",
     ExpressionAttributeValues: {
-      ':attachment': data.attachment || null,
-      ":content": data.content || null
+      ":attachment": {"S":data.attachment || null},
+      ":content": {"S": data.content || null}
     },
     // Can also be ALL_OLD, UPDATED_OLD, UPDATED_NEW 
     // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_UpdateItem.html
@@ -22,6 +24,6 @@ export async function main (event, context, callback) {
     await dynamoDbLib.call('update', params)
     return (success({ status:true }))
   } catch (e) {
-    return failure({ status: false })
+    return failure({ status: false, params:JSON.stringify(params) })
   }
 }
