9a40414d0d6963d026039c303b06f8b044b0b212
Julian Harris <julian@disco-daddy.lan>
2019-12-22 00:27:25 +0000

    Key mapping fix

--- update.js
+++ update.js
@@ -4,17 +4,19 @@ import { success, failure } from './libs/response-lib'
 export async function main (event, context) {
   const data = JSON.parse(event.body)
 
-
   const params = {
     TableName: process.env.tableName,
     Key: {
-      userId: {"S": event.requestContext.identity.cognitoIdentityId},
-      noteId: {"S": event.pathParameters.id}
+      // Docs say the values have to have types built-in (e.g. 'userId': {"S":"event…"})
+      // but here it works ok. Putting them in actually causes problems. 
+      // Clearly the Javascript SDK is doing some mapping
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
     },
     UpdateExpression: "SET content = :content, attachment = :attachment",
     ExpressionAttributeValues: {
-      ":attachment": {"S":data.attachment || null},
-      ":content": {"S": data.content || null}
+      ":attachment": data.attachment || null,
+      ":content": data.content || null
     },
     // Can also be ALL_OLD, UPDATED_OLD, UPDATED_NEW 
     // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_UpdateItem.html
@@ -24,6 +26,6 @@ export async function main (event, context) {
     await dynamoDbLib.call('update', params)
     return (success({ status:true }))
   } catch (e) {
-    return failure({ status: false, params:JSON.stringify(params) })
+    return failure({ status: false, exception:JSON.stringify(e) })
   }
 }
